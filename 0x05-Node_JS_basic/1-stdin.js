099e69416cd2bda3db8df07e223fa1fe8a8a0831
MissTipo <dorine.a.tipo@gmail.com>
2023-05-16 11:09:22 +0300

    Change event listener

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', () => {
-  let input; // = process.stdin.read()
-  while ((input === process.stdin.read()) !== null) process.stdout.write(`Your name is ${input}`);
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
+ // process.exit();
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
