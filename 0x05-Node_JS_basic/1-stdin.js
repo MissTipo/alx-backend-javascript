7f2ee13f3518c8b217b75f0cfabc5796e3380835
MissTipo <dorine.a.tipo@gmail.com>
2023-05-16 11:10:43 +0300

    Eslint fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   process.stdout.write(`Your name is: ${input}`);
- // process.exit();
+  // process.exit();
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
