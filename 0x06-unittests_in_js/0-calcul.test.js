bab9c71b31d764f84cea33c2238aa011c80ebd79
MissTipo <dorine.a.tipo@gmail.com>
2023-05-19 06:00:19 +0300

    add description

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,8 +2,10 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  assert.strictEqual(calculateNumber(1, 3), 4);
-  assert.strictEqual(calculateNumber(1, 3.7), 5);
-  assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('should return rounded sum', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
