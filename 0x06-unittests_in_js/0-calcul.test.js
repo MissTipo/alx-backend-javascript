a1e95b90b4bd253970204d1dad08b67cdf8d1bb4
MissTipo <dorine.a.tipo@gmail.com>
2023-05-19 05:35:14 +0300

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  assert.strictEqual(calculateNumber(1, 3), 4);
+  assert.strictEqual(calculateNumber(1, 3.7), 5);
+  assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+});
