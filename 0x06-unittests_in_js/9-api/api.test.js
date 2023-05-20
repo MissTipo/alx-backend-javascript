8199365f90eada5ce486c74d0293471c1669e9c7
MissTipo <dorine.a.tipo@gmail.com>
2023-05-20 20:46:30 +0300

    Revert "Add test suite for id is not a number"
    This reverts commit b4759663909b6ccef61c1280b9ea76421752ed7f.

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -13,34 +13,19 @@ describe('Basic integration testing', () => {
         expect(res.statusCode).to.equal(200);
         expect(body).to.equal('Welcome to the payment system')
         done();
-      });
-    });
-  });
-});
-describe('Regex integration testing', () => {
-  describe('GET /', () => {
-    it('endpoint GET /cart/:id', (done) => {
-      const options = {
-        method: 'GET',
-        url: 'http://localhost:7865/cart/12',
-      };
-      request(options, (err, res, body) => {
-        expect(res.statusCode).to.equal(200);
-        expect(body).to.equal('Payment methods for cart: id')
-        done();
-      });
-    });
-  });
-  describe('GET /cart/:isNaN', () => {
-    it('endpoint GET /cart/:isNaN', (done) => {
-      const options = {
-        method: 'GET',
-        url: 'http://localhost:7865/cart/12',
-      };
-      request(options, (err, res, body) => {
-        expect(res.statusCode).to.equal(404);
-        expect(body).to.equal('Payment methods for cart: id')
-        done();
+        describe('GET /', () => {
+          it('endpoint GET /cart/:id', (done) => {
+            const options = {
+              method: 'GET',
+              url: 'http://localhost:7865/cart/12',
+            };
+            request(options, (err, res, body) => {
+              expect(res.statusCode).to.equal(404);
+              expect(body).to.equal('Payment methods for cart: id')
+              done();
+            });
+          });
+        });
       });
     });
   });
