b835bf804144504410077f0598500ec9f7599181
MissTipo <dorine.a.tipo@gmail.com>
2023-05-20 19:17:45 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Basic integration testing', () => {
+  describe('GET /', () => {
+    it('endpoint GET /', (done) => {
+      const options = {
+        method: 'GET',
+        url: 'http://localhost:7865',
+      };
+      request(options, (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system')
+        done();
+        describe('GET /', () => {
+          it('endpoint GET /cart/:id', (done) => {
+            const options = {
+              method: 'GET',
+              url: 'http://localhost:7865/cart/12',
+            };
+            request(options, (err, res, body) => {
+              expect(res.statusCode).to.equal(200);
+              expect(body).to.equal('Payment methods for cart: id')
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+});
