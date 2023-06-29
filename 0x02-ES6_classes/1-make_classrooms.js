e13af3ef96e7be1f195e9fbfae390c96ffda55d0
MissTipo <dorine.a.tipo@gmail.com>
2023-06-29 13:28:12 +0300

    fix eslint errror

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,5 @@
 // import ClassRoom class from 0-classroom.js
 // Implement a function named initializeRooms. It should return an array of 3 ClassRoom objects with the sizes 19,20,34(in this order).
-
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
