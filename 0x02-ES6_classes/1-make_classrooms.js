3c153fe9379f25a03c3bc5527264b44324bc7a60
MissTipo <dorine.a.tipo@gmail.com>
2023-06-29 13:23:15 +0300

    Implement a function named initializeRooms that return an array of 3 ClassRoom objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,17 @@
+// import ClassRoom class from 0-classroom.js
+// Implement a function named initializeRooms. It should return an array of 3 ClassRoom objects with the sizes 19,20,34(in this order).
+
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const roomSizes = [19, 20, 34];
+  const classRooms = [];
+
+  for (const roomSize of roomSizes) {
+    const classRoom = new ClassRoom(roomSize);
+    classRooms.push(classRoom);
+  }
+  return classRooms;
+}
+
+module.exports = initializeRooms;
